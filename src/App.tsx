4543ab38285bedf37e0a724a355cfe07b1215338
Vitor-C-Souza <vitor_souza97@hotmail.com>
2025-01-28 13:39:29 -0300

    Implementado as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
-
-import { BrowserRouter, Routes } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Produto from "./pages/produto/Produto";
+import Cadastro from "./pages/cadastro/Cadastro";
 
 function App() {
   return (
@@ -11,9 +13,16 @@ function App() {
       <BrowserRouter>
         <div>
           <Navbar />
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<About />} />
+            <Route path="/produto" element={<Produto />} />
+            <Route path="/cadastro-funcionario" element={<Cadastro />} />
+          </Routes>
         </div>
         <Footer />
       </BrowserRouter>
     </>
+  );
+}
 export default App;
